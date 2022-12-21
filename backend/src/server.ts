7ad40c10337a1c3f3d018dc13a7159c253a6095d
Jeremy Tupas <jeremytupas@gmail.com>
2022-12-20 21:03:39 -0600

    Set up backend

---
+++ backend/src/server.ts
@@ -0,0 +1,16 @@
+require("dotenv").config();
+require("colors");
+import express from "express";
+import cors from "cors";
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Hello from the backend");
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
